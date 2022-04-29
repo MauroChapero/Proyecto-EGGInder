3d674edc702148df72302040a1b7db5f12dd34ed
MauroChapero <chaperomauro1803@gmail.com>
2022-04-29 00:06:41 -0300

    Commit fixeando las ramas [tomas], Agregado /repositorios [MateriaRepositorio], /servicios [MateriaServicio]

---
+++ Proyecto EGGInder/src/main/java/com/proyecto/egginder/repositorios/MateriaRepositorio.java
@@ -0,0 +1,15 @@
+package com.proyecto.egginder.repositorios;
+
+import com.proyecto.egginder.entidades.Materia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MateriaRepositorio extends JpaRepository<Materia,String> {
+    
+    @Query("SELECT c FROM Materia c WHERE c.nombre = :nombre") 
+    public Materia buscarPorNombre(@Param("nombre") String nombre); 
+    
+}
---
+++ Proyecto EGGInder/src/main/java/com/proyecto/egginder/servicios/MateriaServicio.java
@@ -0,0 +1,74 @@
+package com.proyecto.egginder.servicios;
+
+import com.proyecto.egginder.entidades.Materia;
+import com.proyecto.egginder.repositorios.MateriaRepositorio;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MateriaServicio {
+    
+    @Autowired
+    MateriaRepositorio materiaRepository;
+    
+    @Transactional
+    public Materia crearMateria(String nombre) throws Exception {
+        
+        validarMateria(nombre);
+        Materia materia = new Materia();
+        materia.setNombre(nombre);
+        
+        return materiaRepository.save(materia);
+    }
+    
+    @Transactional
+    public Materia editarMateria(String nombre) throws Exception {
+        validarMateria(nombre);
+        Optional <Materia> opt = materiaRepository.findById(id);
+        
+        if(opt.isPresent()) {
+            Materia materia = opt.get();
+            materia.setNombre(nombre);
+            
+            return materiaRepository.save(materia);
+        } else { 
+            throw new Exception("No se halló la materia.");
+        }
+    }
+    
+    @Transactional
+    public void eliminarMateria(String id) throws Exception {
+        Optional<Materia> opt = materiaRepository.findById(id);
+        if (opt.isPresent()) {
+            materiaRepository.deleteById(id);
+        } else {
+            throw new Exception("No se halló la materia a eliminar.");
+        }
+    }
+    
+    public void validarMateria(String nombre) throws Exception {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new Exception("El nombre no puede ser nulo, ni estar vacío.");
+        }
+    }
+    
+    @Transactional
+    public List<Materia> listarMaterias(String nombre) {
+        return materiaRepository.findAll();
+    }
+    
+    @Transactional
+    public Materia buscarPorId(String id) throws Exception {
+        Optional<Materia> opt = materiaRepository.findById(id);
+        if (opt.isPresent()) {
+            Materia materia = opt.get();
+            return materia;
+        } else {
+            throw new Exception("No se halló la materia.");
+        }
+    }
+}
+
